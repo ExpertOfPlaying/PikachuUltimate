9d84dfa21c6a46f9554d96fdefa062440419884d
ExpertOfPlaying <alex-yu2001@yandex.com>
2022-04-29 16:07:29 +0200

    Update Mauzi Prefab, Pikachu Kinematic

--- Assets/Scripts/Prefeber.cs
+++ Assets/Scripts/Prefeber.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Prefeber : MonoBehaviour
 {
-
+    public Transform Hunted;
     public Transform BallPrefab;
+    public Transform MauziPrefab;
     [SerializeField] float startPosY = 1.214343f;
     [SerializeField] float posRange = 50;
     [SerializeField] float eulerRange = 360;
     [SerializeField] float maxBalls = 20;
+    [SerializeField] float maxMauzis = 3;
+
 
     private Vector3 posPrefab;
     private Quaternion rotPrefab;
@@ -23,5 +26,14 @@ public class Prefeber : MonoBehaviour
             rotPrefab = Quaternion.Euler(0, Random.Range(-eulerRange, eulerRange), 0);
             Transform tmppokeball = Instantiate(BallPrefab, posPrefab, rotPrefab);
         }
+
+        for (int i = 0; i < maxMauzis; i++)
+        {
+            posPrefab = new Vector3(Random.Range(-posRange, posRange), startPosY, Random.Range(-posRange, posRange));
+            rotPrefab = Quaternion.Euler(0, Random.Range(-eulerRange, eulerRange), 0);
+            Transform tmpMauzi = Instantiate(MauziPrefab, posPrefab, MauziPrefab.rotation);
+            tmpMauzi.GetComponent<Hunter>().Player = Hunted;
+        }
+
     }
 }
